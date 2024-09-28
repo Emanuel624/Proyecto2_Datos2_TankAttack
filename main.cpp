9e603e91079c336b595073a0b68cb6e7ff573452
Emanuel624 <emanuel.chavarria.hernandez@gmail.com>
2024-09-28 01:31:56 -0600

    Logica extremadamente-basica de QT

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include <QApplication>
+#include <QWidget>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);  // Inicia la aplicación Qt
+
+    QWidget window;  // Crea una ventana básica
+    window.resize(250, 150);  // Establece el tamaño de la ventana
+    window.setWindowTitle("Ventana Basica");  // Establece el título de la ventana
+    window.show();  // Muestra la ventana
+
+    return app.exec();  // Inicia el bucle de eventos de Qt
 }
